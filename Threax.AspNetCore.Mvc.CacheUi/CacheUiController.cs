f291a375c0a97811501ef821bdced4e5f5bcbd0e
Andrew Piper <andrew@threax.com>
2020-07-17 10:44:40 -0400

    Added comments.

--- Threax.AspNetCore.Mvc.CacheUi/CacheUiController.cs
+++ Threax.AspNetCore.Mvc.CacheUi/CacheUiController.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Threax.AspNetCore.Mvc.CacheUi
 {
+    /// <summary>
+    /// You can inherit from this controller to gain the CacheUiView helper method.
+    /// This is optional. You can also use ICacheUiBuilder directly.
+    /// </summary>
     public class CacheUiController : Controller
     {
         public CacheUiController(ICacheUiBuilder builder)
@@ -17,6 +21,7 @@ namespace Threax.AspNetCore.Mvc.CacheUi
 
         public async Task<IActionResult> CacheUiView(string view = null, object model = null, string cacheToken = null)
         {
+            //This method is in a different order from the rest of the api.
             var result = await this.CacheUiBuilder.Build(this, cacheToken: cacheToken, view: view, model: model);
             return result.ActionResult;
         }
